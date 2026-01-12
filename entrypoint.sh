59fe0fdbfcbe60c2b26748601a075880d9aa3fcc
Cooper Keenan <40595321@live.napier.ac.uk>
2026-01-12 15:36:37 +0000

    removed redundant files

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Run script and write to BOTH stdout and file
-python -u test_navigation.py 2>&1 | tee /app/logs/output.log
-
-echo "=== Log file created at /app/logs/output.log ===" | tee -a /app/logs/output.log
